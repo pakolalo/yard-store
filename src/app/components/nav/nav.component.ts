b2c02d7164eea22f748d6e36ec5b19ee5350f71b
Francisco del Collado <fedelcollado@gmail.com>
2024-03-13 22:51:02 -0600

    added products to shopping cart

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Product } from '../../models/product.model';
+import { StoreService } from '../../services/store.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,9 +11,20 @@ import { Component } from '@angular/core';
 })
 export class NavComponent {
   activeMenu = false;
+  myShoppingCart: Product[] =[]
+
+  constructor(
+    private storeService: StoreService,
+  ) {
+    this. myShoppingCart = this.storeService.getShoppingCart();
+  }
 
   toggleNavBar() {
     this.activeMenu = !this.activeMenu;
+  }
+
+  addToShoppingCart(product: Product) {
+    this.storeService.addProduct(product)
   }
 
 }
